1261efa0d5eec24e7bbd1c428308c20f12d60c7a
jrfernandez <jrfernandezc03@educarex.es>
2021-03-07 13:03:45 +0100

    Validamos Cambio2 en proyecto2 8º commit

--- proyecto2/src/proyecto2/Cambio2.java
+++ proyecto2/src/proyecto2/Cambio2.java
@@ -4,7 +4,8 @@ public class Cambio2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Clase Cambio2 en proyecto2");
+		System.out.println("Clase Cambio2 en proyecto2");
 	}
 
 }
