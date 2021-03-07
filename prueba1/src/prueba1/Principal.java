43216eaf4fefc818ed3402f22958da3c915a631b
jrfernandez <jrfernandezc03@educarex.es>
2021-03-07 10:39:55 +0100

    Primera modificación clase Principal, 2º commit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo java-git");
+		System.out.println("2 Hola mundo java-gi");
 
 	}
 
